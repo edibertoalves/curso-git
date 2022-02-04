3f70a15ffc2011a472185588fbcbcfd5310ed36a
Ediberto Alves <ediberto@gmail.com>
2022-02-04 16:16:46 -0300

    Projeto iniciado mostrando a mensagem Ola Mundo

---
+++ projeto-teste/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto teste");
+
+            Console.ReadLine();
+        }
+    }
+}
